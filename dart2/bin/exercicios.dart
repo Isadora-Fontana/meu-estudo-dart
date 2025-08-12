3555a20bf0bb1ad869e80e06e4512f4356810e43
Isadora-Fontana <isadorafontana.so@gmail.com>
2025-08-12 11:01:29 -0400

    Finalizei o segundo mini módulo

--- dart2/bin/exercicios.dart
+++ dart2/bin/exercicios.dart
@@ -126,20 +126,101 @@
 
 
 
-void main(){
-  Set<String> produtosVendidos = {"maçã", "banana", "laranja", "kiwi"};   
-  Set<String> produtosEstoque = {"banana", "kiwi", "laranja", "pera", "melão", "melancia"}; 
+// void main(){
+//   Set<String> produtosVendidos = {"maçã", "banana", "laranja", "kiwi"};   
+//   Set<String> produtosEstoque = {"banana", "kiwi", "laranja", "pera", "melão", "melancia"}; 
 
-  verificadorEstoque(produtosVendidos, produtosEstoque);
+//   verificadorEstoque(produtosVendidos, produtosEstoque);
+// }
+
+// verificadorEstoque(Set<String> vendidos, Set<String> estoque){
+//   Set<String> naoVendidos = {};
+
+//   for (String produto in estoque){
+//     if (!vendidos.contains(produto)){
+//       naoVendidos.add(produto);
+//     }
+//   }
+//   print("Os produtos não vendidos são $naoVendidos");
+// }
+
+
+// void main(){
+//   Map<String, double> precosProdutos = { 
+//     "Camiseta": 30.0, 
+//     "Calça": 50.0, 
+//     "Boné": 15.0, 
+//     "Tênis": 120.0 
+//   }; 
+
+//   print(calcularTotal(precosProdutos));
+// }
+
+// double calcularTotal(Map<String, double> precosProdutos){
+//   double total = 0;
+//   for (double preco in precosProdutos.values){
+//     total += preco;
+//   }
+//   return total;
+// }
+
+
+// import 'dart:io';
+// void main(){
+//   Map<String, double> inventarioProdutos = { 
+//     "Camiseta": 30.0, 
+//     "Calça": 50.0, 
+//     "Boné": 15.0 
+//   }; 
+
+//   print("Produtos disponíveis:");
+//   inventarioProdutos.forEach((produto, preco) {
+//     print("$produto - R\$${preco.toStringAsFixed(2)}");
+//   });
+
+//   stdout.write("\nDigite o nome do produto que deseja atualizar: ");
+//   String? produto = stdin.readLineSync();
+
+//   stdout.write("Digite o novo preço: ");
+//   String? precoStr = stdin.readLineSync();
+//   double? novoPreco = double.tryParse(precoStr ?? "");
+
+//   if (produto != null && novoPreco != null) {
+//     atualizarPreco(inventarioProdutos, produto, novoPreco);
+//   } else {
+//     print("Entrada inválida.");
+//   }
+// }
+
+// void atualizarPreco(Map<String, double> inventarioProdutos, String produto, double novoPreco) { 
+
+//   if (inventarioProdutos.containsKey(produto)) { 
+//     inventarioProdutos[produto] = novoPreco; 
+//     print("Preço atualizado: $produto - R\$${novoPreco.toStringAsFixed(2)}"); 
+
+//   } else { 
+//     print("Produto não encontrado no inventário."); 
+//   } 
+// } 
+
+
+
+
+void main(){
+  List<Map<String, dynamic>> clientes = [ 
+  {"nome": "João", "pontuacao": 6.5}, 
+  {"nome": "Maria", "pontuacao": 8.7}, 
+  {"nome": "Pedro", "pontuacao": 9.2}, 
+  {"nome": "Ana", "pontuacao": 5.4} 
+]; 
+
+clientesFidelidade(clientes);
 }
 
-verificadorEstoque(Set<String> vendidos, Set<String> estoque){
-  Set<String> naoVendidos = {};
-
-  for (String produto in estoque){
-    if (!vendidos.contains(produto)){
-      naoVendidos.add(produto);
+clientesFidelidade(List<Map<String, dynamic>> clientes){
+  for (Map<String, dynamic> cliente in clientes){
+    if (cliente["pontuacao"] > 7 ){
+      print(cliente["nome"]);
     }
   }
-  print("Os produtos não vendidos são $naoVendidos");
 }
