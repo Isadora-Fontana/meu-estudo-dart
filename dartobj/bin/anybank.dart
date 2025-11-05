40f95953ef28b7827d56c22ee85c7c2c5fc40cae
Isadora Fontana Sodré de Oliveira <09122824103@pgemt>
2025-11-05 12:34:15 -0400

    só garantindo que nao esqueci de nada, pc do trabalho

--- dartobj/bin/anybank.dart
+++ dartobj/bin/anybank.dart
@@ -2,6 +2,8 @@ import '../lib/conta.dart';
 void main() {
     ContaCorrente contaChris = ContaCorrente("Chris", 4000);
     ContaPoupanca contaDenize = ContaPoupanca("Denize", 4000);
+    ContaEmpresa contaMatheus = ContaEmpresa('contaMatheus', 2000);  
+    ContaInvestimento contaRoberta = ContaInvestimento('Roberta', 2000);
 
     ContaSalario contaIsadora = ContaSalario("Isadora", 9000, "112334594", "Amagi");
 
@@ -15,5 +17,8 @@ void main() {
 
     contaIsadora.depositarSalario(10000);
     contaIsadora.imprimeSaldo();
+
+    contaMatheus.enviar(1000);
+    contaRoberta.receber(1000);
 
 }
--- dartobj/bin/codigoErro.dart
+++ dartobj/bin/codigoErro.dart
@@ -80,17 +80,112 @@
 
 
 // ex6: Criando um programa informativo de espécies de seres vivos
-abstract class SerVivo{
-  String nome;
-  String tipo;
+// import 'dart:ffi';
+// import 'dart:vmservice_io';
 
-  SerVivo(this.nome, this.tipo);
+// abstract class SerVivo{
+//   String nome;
+//   String tipo;
 
-  void mostrarCaracteristicas(){
+//   SerVivo(this.nome, this.tipo);
 
+//   void mostrarCaracteristicas(){
+//   }
+// }
+
+// class Planta extends SerVivo{
+//   Planta(String nome, String tipo) : super(nome, tipo);
+
+//   @override
+//   void mostrarCaracteristicas(){
+//     print("$nome: é uma planta, não tem os cinco sentidos, é uma $tipo");
+//   }
+// }
+
+// class Animal extends SerVivo{
+//   int tempoVida;
+//   Animal(String nome, String tipo, this.tempoVida) : super(nome, tipo);
+
+//   @override
+//   void mostrarCaracteristicas(){
+//     print('$nome: tem cinco sentidos, pensa e fala, é $tipo e vive em média $tempoVida anos');
+//   }
+// }
+
+// void main(){
+//   Planta girassol = Planta('Girassol', 'flor');
+//   girassol.mostrarCaracteristicas();
+//   Planta laranjeira = Planta('Laranjeira', 'árvore');
+//   laranjeira.mostrarCaracteristicas();
+
+//   Animal serHumano = Animal('Ser Humano', 'bípede', 75);
+//   serHumano.mostrarCaracteristicas();
+//   Animal gato = Animal('Gato', 'quadrúpede', 20);
+//   gato.mostrarCaracteristicas();
+//   Animal cao = Animal('Cão', 'quadrúpede', 20);
+//   cao.mostrarCaracteristicas();
+// }
+
+
+
+//ex7,8,9,10,11: Definindo a interface para agendamento
+abstract class Agendamento{
+
+  void calculaDuracaoConsulta(){
+  }
+
+  void verificaDisponibilidade(){
+    }
+}
+
+class Medico implements Agendamento{
+
+    @override
+    void calculaDuracaoConsulta(){
+    print('Duração de 30 minutos');
+  }
+
+  @override
+  void verificaDisponibilidade(){
+    print('Verificar disponibilidade no calendário');
   }
 }
 
-class Planta implements SerVivo{
-  Planta(String nome) : super(nome, 'planta');
+class Dentista implements Agendamento{
+
+  @override
+  void calculaDuracaoConsulta(){
+    print('duração de 45 minutos');
+  }
+
+  @override
+  void verificaDisponibilidade(){
+    print('verificar disponibilidade considerando intervalos de 15 minutos');
+  }
+}
+
+class gerenciadorDeAgendamentos{
+  List<Agendamento> profissionais = [];
+
+    void adicionarProfissional(Agendamento profissional){
+      profissionais.add(profissional);
+    }
+
+    void exibirDuracaoEDisponiblidade(){
+      for (Agendamento profissional in profissionais){
+        profissional.calculaDuracaoConsulta();
+        profissional.verificaDisponibilidade();
+      }
+    }
+}
+
+void main(){
+  Medico medico = Medico();
+  Dentista dentista = Dentista();
+
+  gerenciadorDeAgendamentos gerenciador = gerenciadorDeAgendamentos();
+  gerenciador.adicionarProfissional(medico);
+  gerenciador.adicionarProfissional(dentista);
+
+  gerenciador.exibirDuracaoEDisponiblidade();
 }
